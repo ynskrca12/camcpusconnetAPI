a044ff07f7d8a71d34e48ddd1633240b48382a6d
ynskrca12 <ynskrca52@outlook.com>
2023-06-10 14:11:40 +0300

    İlanlar API'si yazdıldı.

---
+++ app/Http/Controllers/Api/AdvertController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CreateAdvertRequest;
+use App\Http\Requests\EditAdvertRequest;
+use App\Models\Advert;
+use Exception;
+use Illuminate\Http\Request;
+
+class AdvertController extends Controller
+{
+    public function index(Request $request){
+
+        try {
+            $query = Advert::query();
+            $perPage =1;
+            $page = $request->input('page',1);
+            $search = $request->input('search');
+    
+    
+            if($request){
+                $query->whereRaw("name LIKE '%" . $search . "%'");
+            }
+            
+            $total = $query->count();
+            $result = $query->offset(($page - 1) * $perPage)->limit($perPage)->get();
+
+            return response()->json([
+                'status_code' => 200,
+                'status_message' => 'İlanlar başarıyla listelendi.',
+                'current_page' => $page,
+                'last_page' => ceil($total / $perPage),
+                'items' => $result
+            ]);
+
+        } catch (Exception $e) {
+            return response()->json($e);
+        }
+
+
+
+    }//End Method
+
+    public function store(CreateAdvertRequest $request){
+
+        
+      try{
+        // $file = $request->file('file');
+        // $fileName = $file->getClientOriginalName();
+        // $file->move(public_path('uploads'), $fileName);
+
+        $advert = new Advert();
+
+        $advert->name=$request->name;
+        $advert->description=$request->description;
+        $advert->category=$request->category;
+       // $advert->imagePath=$fileName;
+        $advert->save();
+
+        return response()->json([
+            'status_code' => 200,
+            'status_message' => 'İlan başarıyla kaydedildi.',
+            'data' =>$advert
+        ]);
+      }
+      catch (Exception $e){
+        return response()->json($e);
+      }
+
+    } //End Method
+    
+    public function update(EditAdvertRequest $request, Advert $advert){
+
+    try{        
+     
+        $advert->name=$request->name;
+        $advert->description=$request->description;
+        $advert->category=$request->category;
+    
+        $advert->save();
+
+        return response()->json([
+            'status_code' => 200,
+            'status_message' => 'ilan başarıyla güncellendi.',
+            'data' =>$advert
+        ]);
+    }
+    catch (Exception $e) {
+        return response()->json($e);
+    }
+
+
+    }//End Method
+
+    public function delete(Advert $advert){
+        try {
+            if($advert){
+                $advert->delete();
+
+            return response()->json([
+                'status_code' => 200,
+                'status_message' => 'İlan başarıyla silindi.',
+                'data' =>$advert
+            ]);
+            }else{
+
+            return response()->json([
+                'status_code' => 422,
+                'status_message' => 'İlan bulunamadı.',
+            ]);
+            }
+
+        } catch (Exception $e) {
+            return response()->json($e);
+        }
+    }//End Method
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\ActivityController;
+use App\Http\Controllers\Api\AdvertController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,6 +9,13 @@ Route::get('activities',[ActivityController::class,'index']);
 Route::post('activities/create',[ActivityController::class,'store']);
 Route::put('activities/edit{activity}',[ActivityController::class,'update']);
 Route::delete('activities/{activity}',[ActivityController::class,'delete']);
+
+
+Route::get('adverts',[AdvertController::class,'index']);
+Route::post('adverts/create',[AdvertController::class,'store']);
+Route::put('adverts/edit{activity}',[AdvertController::class,'update']);
+Route::delete('adverts/{activity}',[AdvertController::class,'delete']);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
